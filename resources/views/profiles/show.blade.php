8207cfaad336f564a7e670280819812dffbc0029
Matteoverzotti <verzotti_matteo_9e-21@lbiro.onmicrosoft.com>
2023-09-28 14:11:56 +0300

    Edited profile page

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -3,15 +3,30 @@
 <h1>{{ $profile->name }}</h1>
 <p>Graduation Year: {{ $profile->graduation_year }}</p>
 
-<h2>Participations:</h2>
-<ul>
-    @foreach ($profile->contestants as $participation)
-        @php
-            $sub_contest = App\Models\SubContest::find($participation->sub_contest_id);
-            $contest = App\Models\Contest::find($sub_contest->contest_id);
-        @endphp
-        <li><a
-                href="{{ route('sub_contests.show', ['name_id' => $contest->name_id, 'sub_contest_name_id' => $sub_contest->name_id]) }}">
-                {{ $sub_contest->name }} </a></li>
-    @endforeach
-</ul>
+<table>
+    <thead>
+        <tr>
+            <th>Competiție</th>
+            <th>Loc</th>
+            <th>Medalie</th>
+            <th>Premiu</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($profile->contestants as $participation)
+            @php
+                $sub_contest = App\Models\SubContest::find($participation->sub_contest_id);
+                $contest = App\Models\Contest::find($sub_contest->contest_id);
+                $contestant = $sub_contest->contestants->where('profile_id', $profile->id)->first();
+            @endphp
+            <tr>
+                <td><a
+                        href="{{ route('sub_contests.show', ['name_id' => $contest->name_id, 'sub_contest_name_id' => $sub_contest->name_id]) }}">
+                        {{ $sub_contest->name }}</a></td>
+                <td>{{$contestant->place}}</td>
+                <td>{{$contestant->medal}}</td>
+                <td>{{$contestant->prize}}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
--- resources/views/sub_contests/show.blade.php
+++ resources/views/sub_contests/show.blade.php
@@ -5,29 +5,27 @@
 <p>{{ $sub_contest->date }}</p>
 
 <h2>Participants</h2>
-<ul>
-    <table>
-        <thead>
+<table>
+    <thead>
+        <tr>
+            <th>Loc</th>
+            <th>Nume</th>
+            <th>Echipă</th>
+            <th>Județ</th>
+            <th>Medalie</th>
+            <th>Premiu</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($sub_contest->contestants as $contestant)
             <tr>
-                <th>Loc</th>
-                <th>Nume</th>
-                <th>Echipă</th>
-                <th>Județ</th>
-                <th>Medalie</th>
-                <th>Premiu</th>
+                <td>{{$contestant->place}}</td>
+                <td><a href="{{ route('profiles.show', ['id' => $contestant->profile_id]) }}">{{ $contestant->name }}</td>
+                <td>{{$contestant->team}}</td>
+                <td>{{$contestant->region}}</td>
+                <td>{{$contestant->medal}}</td>
+                <td>{{$contestant->prize}}</td>
             </tr>
-        </thead>
-        <tbody>
-            @foreach ($sub_contest->contestants as $contestant)
-                <tr>
-                    <td>{{$contestant->place}}</td>
-                    <td><a href="{{ route('profiles.show', ['id' => $contestant->profile_id]) }}">{{ $contestant->name }}</td>
-                    <td>{{$contestant->team}}</td>
-                    <td>{{$contestant->region}}</td>
-                    <td>{{$contestant->medal}}</td>
-                    <td>{{$contestant->prize}}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</ul>
+        @endforeach
+    </tbody>
+</table>
