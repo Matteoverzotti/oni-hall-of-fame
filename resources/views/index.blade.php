ebdc9dfef1575512fc10390738157b644131eaf8
Matteoverzotti <verzotti_matteo_9e-21@lbiro.onmicrosoft.com>
2023-10-28 15:36:48 +0300

    Changed default page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,52 +10,73 @@
 <body>
     @include('partials._navbar')
 
-
     <h1
         class="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         Bine ai venit la ONI Vault!
     </h1>
-    <p class="mb-6 text-lg text-center font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-        Ultimele concursuri
-    </p>
 
-    @if (count($contests) == 0)
-    <h2>No contests found!</h2>
-    @else
+    <div class="flex flex-col items-center min-h-screen">
+    {{-- Two columns div --}}
+        <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 p-10">
+            {{-- First column --}}
+            <div>
+                <p
+                    class="mb-6 text-lg text-center font-normal text-gray-700 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+                    Ce este ONI-Vault?
+                </p>
+                <p class="mb-3 text-gray-700 dark:text-gray-400">
+                    {{ Lipsum::short()->text(3) }}
+                </p>
+            </div>
 
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <x-th>
-                        Nume
-                    </x-th>
-                    <x-th>
-                        <div class="flex items-center">
-                            Locație
-                        </div>
-                    </x-th>
-                    <x-th>
-                        <div class="flex items-center">Dată</div>
-                    </x-th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($contests as $contest)
-                @if ($loop->index < 5) <x-tr>
-                    <x-td><a href="{{ route('contests.show', ['name_id' => $contest->name_id]) }}">{{ $contest->name
-                            }}</a></x-td>
-                    <x-td>{{ $contest->location }}</x-td>
-                    <x-td>{{ $contest->date }}</x-td>
-                    </x-tr>
-                    @else
-                    @break
-                    @endif
-                    @endforeach
-            </tbody>
-        </table>
+            {{-- Second column --}}
+            <div>
+                <p
+                    class="mb-6 text-lg text-center font-normal text-gray-700 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+                    Ultimele concursuri
+                </p>
+
+                @if (count($contests) == 0)
+                    <h2>No contests found!</h2>
+                @else
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <x-th>
+                                        Nume
+                                    </x-th>
+                                    <x-th>
+                                        <div class="flex items-center">
+                                            Locație
+                                        </div>
+                                    </x-th>
+                                    <x-th>
+                                        <div class="flex items-center">Dată</div>
+                                    </x-th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($contests as $contest)
+                                    @if ($loop->index >= 5)
+                                        @break
+                                    @endif
+
+                                <x-tr>
+                                    <x-td><a
+                                            href="{{ route('contests.show', ['name_id' => $contest->name_id]) }}">{{ $contest->name }}</a></x-td>
+                                    <x-td>{{ $contest->location }}</x-td>
+                                    <x-td>{{ $contest->date }}</x-td>
+                                </x-tr>
+                                @endforeach
+                                </tbody>
+                        </table>
+                    </div>
+                @endif
+            </div>
+        </div>
     </div>
-    @endif
 </body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors'); // Add this import
+
 module.exports = {
     content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-      "./node_modules/flowbite/**/*.js"
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
     theme: {
-      extend: {},
+        colors: {
+            gray: colors.coolGray,
+            red: colors.red,
+            yellow: colors.amber,
+            green: colors.emerald,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            purple: colors.violet,
+            pink: colors.pink,
+        },
+        extend: {},
     },
-    plugins: [
-        require('flowbite/plugin')
-    ],
-}
+    plugins: [require("flowbite/plugin")],
+};
