1d0fa1500235611c62e80751b88d0b07d28b29de
Matteoverzotti <verzotti_matteo_9e-21@lbiro.onmicrosoft.com>
2023-12-19 21:05:21 +0200

    Added development database

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION_DEV', 'mysql_dev'),
 
     /*
     |--------------------------------------------------------------------------
@@ -51,6 +51,26 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        'mysql_dev' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST_DEV', '127.0.0.1'),
+            'port' => env('DB_PORT_DEV', '3306'),
+            'database' => env('DB_DATABASE_DEV', 'forge'),
+            'username' => env('DB_USERNAME_DEV', 'forge'),
+            'password' => env('DB_PASSWORD_DEV', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
