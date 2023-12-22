b9b0eaf4e30bc37743b8ff7fd84f92e944735f5a
Matteoverzotti <verzotti_matteo_9e-21@lbiro.onmicrosoft.com>
2023-12-22 23:50:21 +0200

    Deleted button component

--- resources/views/components/button.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- resources/views/components/button.blade.php -->
-
-@props(['type', 'class', 'onclick'])
-
-<button type="{{ $type }}" class="{{ $class }}" @if(isset($onclick)) onclick="{{ $onclick }}" @endif>
-    {{ $slot }}
-</button>
